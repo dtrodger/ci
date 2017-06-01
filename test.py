8881deedfeaf095ef1fec9fd19e0ec1d434ac338
David Rodgers <dtrodger20@gmail.com>
2017-05-31 20:31:52 -0500

    Add False test.

--- test.py
+++ test.py
@@ -4,7 +4,9 @@ class Test(unittest.TestCase):
 
 	def test_true(self):
 		self.assertTrue(True)
-		print "test"
+
+	def test_false(self):
+		self.assertFalse(False)
 
 if __name__ == '__main__':
 	unittest.main()
