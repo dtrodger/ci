ad96bba3504f28a02539da4cdf14a3da7ce3f3fc
David Rodgers <dtrodger20@gmail.com>
2017-06-08 23:40:39 -0400

    Add print statement

--- test.py
+++ test.py
@@ -3,9 +3,14 @@ import unittest
 class Test(unittest.TestCase):
 
 	def test_true(self):
-		print "HI"
+		print "HELLO"
 		self.assertTrue(True)
 
 if __name__ == '__main__':
 	unittest.main()
+
+# Docker w/ nginx
+# output status of test to web app
+# deploy rails app to server
+# tool that mocks interface of sqs so in unittest we are not sending request to amazon, but send reuqest to app or service thats running locally to imitate sqs
 	
